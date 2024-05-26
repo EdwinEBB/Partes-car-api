f03a5ea3bc91069758b51549f31dd430b608cdf8
Edwin Enrique Bayuelo Bello <141866369+EdwinEBB@users.noreply.github.com>
2024-05-26 16:05:37 -0500

    correciones 4

--- routes/improvement.routes.js
+++ routes/improvement.routes.js
@@ -4,7 +4,6 @@ import { createImprovement, deleteImprovementById, getAllImprovements, getImprov
 
 const improvementroutes=express.Router();
 
-improvementroutes=express.Router();
 tuningroutes.get('/',(req,res)=>{
     res.send(`
     <H1>HOLA ESTAS EN IMPROMENT</H1>`);
